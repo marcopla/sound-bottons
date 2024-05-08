fa51c1225ff09492ec133d5f1ef573573eeebadf
Marco Pla <marco.pla@gmail.com>
2024-05-07 23:50:15 -0300

    for condition and anonymous function

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+  document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let cont = 0; cont < listaDeTeclas.length; cont++) {
+  const tecla = listaDeTeclas[cont];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+    console.log(instrumento);
+  };
+}

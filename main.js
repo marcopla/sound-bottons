cedf99c8db4e196282fa21a0aedff3ed796e7742
Marco Pla <marco.pla@gmail.com>
2024-05-08 23:33:47 -0300

    event and null verification

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-function tocaSom(idElementoAudio) {
-  document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+
+  if (elemento && elemento.localName == 'audio') {
+    elemento.play();
+  } else {
+    console.log('Elemento não encontrado ou seletor não encontrado.');
+  }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -8,8 +14,21 @@ for (let cont = 0; cont < listaDeTeclas.length; cont++) {
   const tecla = listaDeTeclas[cont];
   const instrumento = tecla.classList[1];
   const idAudio = `#som_${instrumento}`;
+
   tecla.onclick = function () {
     tocaSom(idAudio);
     console.log(instrumento);
+  };
+
+  tecla.onkeydown = function (evento) {
+    if (evento.code === 'Space' || evento.code === 'Enter') {
+      tecla.classList.add('ativa');
+    }
+  };
+
+  tecla.onkeyup = function (evento) {
+    if (evento.code === 'Space' || evento.code === 'Enter') {
+      tecla.classList.remove('ativa');
+    }
   };
 }
